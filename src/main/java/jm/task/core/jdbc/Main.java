938b80a61acec2e827a592b1bca98c3f7ea4c4ec
Denis0872 <denispetergof@gmail.com>
2022-07-15 00:15:02 +0300

    1

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,18 @@
 package jm.task.core.jdbc;
 
+import com.mysql.cj.jdbc.MysqlDataSourceFactory;
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.sql.Connection;
+import java.sql.Driver;
+
 public class Main {
+    private static final UserService userService = new UserServiceImpl();
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        userService.createUsersTable();
+
+
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,11 @@
 package jm.task.core.jdbc.util;
 
+import org.hibernate.SessionFactory;
+
 public class Util {
+    public static SessionFactory getConnection() {
+
+        return null;
+    }
     // реализуйте настройку соеденения с БД
 }
